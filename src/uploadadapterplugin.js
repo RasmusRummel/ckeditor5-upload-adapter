ade8ce1d00f651b9c768c0dc9abe9fffa8fa4968
Rasmus Rummel <rasmusrummel@gmail.com>
2019-05-02 15:07:16 +0700

    Create uploadadapterplugin.js

---
+++ src/uploadadapterplugin.js
@@ -0,0 +1,25 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import FileRepository from '@ckeditor/ckeditor5-upload/src/filerepository';
+
+import UploadAdapter from './uploadadapter';
+
+export default class UploadAdapterPlugin extends Plugin {
+    static get requires() {
+        return [FileRepository];
+    }
+
+    init() {
+        const uploadUrl = this.editor.config.get('uploadAdapter.uploadUrl');
+        const csrfToken = this.editor.config.get['uploadAdapter.csrfToken'];
+        const t = this.editor.t;
+
+        if (!uploadUrl) {
+            console.warn('CKEditor5 FileAdapter : uploadUrl is not defined.');
+            return;
+        }
+
+        this.editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+            return new UploadAdapter(loader, uploadUrl, csrfToken, t);
+        };
+    }
+}
