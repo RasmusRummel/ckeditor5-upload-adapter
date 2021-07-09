370d159fc23060dc702c9d56b687d996f113d874
Rasmus Rummel <rasmusrummel@gmail.com>
2021-07-09 21:44:38 +0700

    Update uploadadapter.js

--- src/uploadadapter.js
+++ src/uploadadapter.js
@@ -1,73 +1,26 @@
-export default class UploadAdapter {
-    constructor(loader, uploadUrl, headers, t) {
-        this.loader = loader;
-        this.uploadUrl = uploadUrl;
-        this.headers = headers;
-        this.t = t;
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import FileRepository from '@ckeditor/ckeditor5-upload/src/filerepository';
+
+import UploadAdapterImplementation from './uploadadapterimplementation';
+
+export default class UploadAdapter extends Plugin {
+    static get requires() {
+        return [FileRepository];
     }
 
-    upload() {
-        return this.loader.file
-            .then(file => new Promise((resolve, reject) => {
-                this._initRequest();
-                this._initListeners(resolve, reject, file);
-                this._sendRequest(file);
-            }));
-    }
+    init() {
+        const uploadUrl = this.editor.config.get('uploadAdapter.uploadUrl');
+        const useFetch = this.editor.config.get('uploadAdapter.useFetch');
+        const headers = this.editor.config.get('uploadAdapter.headers');
+        const t = this.editor.t;
 
-    abort() {
-        if (this.xhr) {
-            this.xhr.abort();
-        }
-    }
-
-    _initRequest() {
-        const xhr = this.xhr = new XMLHttpRequest();
-        xhr.open('post', this.uploadUrl, true);
-        xhr.responseType = 'json';
-    }
-
-    _initListeners(resolve, reject, file) {
-        const xhr = this.xhr;
-        const loader = this.loader;
-        const genericErrorText = "Couldn\'t upload file: " + file.name + ".";
-
-        xhr.addEventListener('error', () => reject(genericErrorText));
-        xhr.addEventListener('abort', () => reject());
-        xhr.addEventListener('load', () => {
-            const response = xhr.response;
-
-            if (!response || response.error) {
-                return reject(response && response.error ? response.error.message : genericErrorText);
-            }
-
-            resolve({
-                default: response.url
-            });
-        });
-
-        if (xhr.upload) {
-            xhr.upload.addEventListener('progress', evt => {
-                if (evt.lengthComputable) {
-                    loader.uploadTotal = evt.total;
-                    loader.uploaded = evt.loaded;
-                }
-            });
-        }
-    }
-
-    _sendRequest(file) {
-        const data = new FormData();
-
-        data.append('upload', file);
-
-        if (this.headers) {
-            var headers = Object.keys(this.headers);
-            for (var h = 0; h < headers.length; h++) {
-                this.xhr.setRequestHeader(headers[h], this.headers[headers[h]]);
-            }
+        if (!uploadUrl) {
+            console.warn('CKPro UploadAdapter : uploadUrl is not defined.');
+            return;
         }
 
-        this.xhr.send(data);
+        this.editor.plugins.get('FileRepository').createUploadAdapter = (fileLoader) => {
+            return new UploadAdapterImplementation(fileLoader, uploadUrl, useFetch, headers, t);
+        };
     }
 }
